0e970216e5c6037882e5a458da188a8671b3a90e
antoniojunio <juniordsj@gmail.com>
2024-11-21 19:35:01 -0300

    Initial commit

---
+++ src/main/java/io/github/levva/floworder/FloworderApplication.java
@@ -0,0 +1,13 @@
+package io.github.levva.floworder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloworderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloworderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/levva/floworder/FloworderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.levva.floworder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloworderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
